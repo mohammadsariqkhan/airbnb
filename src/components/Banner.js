3d367ab210d1111e90265dae81f33a7eb951675c
Mohammad Sariq Khan <mohsariqkhan2004@gmail.com>
2023-06-04 18:19:44 +0530

    ui

---
+++ src/components/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from "next/image";
+
+function Banner(props) {
+    return (
+        <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
+            <Image className="object-cover" src='https://links.papareact.com/0fm' fill/>
+            <div className="absolute top-1/2 w-full text-center">
+                <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+                <button className="bg-white text-purple-500 px-10 py-4 rounded-full shadow-md my-3 font-bold hover:shadow-xl active:scale-x-90 transition duration-150">I'm flexible</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function Footer(props) {
+    return (<div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+        <div className="space-y-4 text-xs text-gray-600">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investing</p>
+            <p>Airbnb plus</p>
+            <p>Airbnb luxe</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-600">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investing</p>
+            <p>Airbnb plus</p>
+            <p>Airbnb luxe</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-600">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investing</p>
+            <p>Airbnb plus</p>
+            <p>Airbnb luxe</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-600">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>Newsroom</p>
+            <p>Investing</p>
+            <p>Airbnb plus</p>
+            <p>Airbnb luxe</p>
+        </div>
+
+    </div>);
+}
+
+export default Footer;
