cb4dcaec120ecd85830e25ff177f955698d57c27
Mohammad Sariq Khan <mohsariqkhan2004@gmail.com>
2023-06-04 18:29:16 +0530

    add alt

--- src/components/LargeCard.js
+++ src/components/LargeCard.js
@@ -5,7 +5,7 @@ function LargeCard({img,title,des,buttonText}) {
     return (
         <section className="relative py-16 cursor-pointer">
             <div className="relative h-96 min-w-[300px]">
-                <Image className="object-cover rounded-2xl" src={img} fill />
+                <Image className="object-cover rounded-2xl" src={img} fill alt='image' />
             </div>
             <div className="absolute top-32 left-12">
                 <h3 className="text-4xl mb-3 w-64">{title}</h3>
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -5,7 +5,7 @@ function SmallCard({img,location,distance}) {
     return (
         <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out ">
             <div className="relative h-16 w-16 ">
-                <Image className="rounded-lg" src={img} fill/>
+                <Image className="rounded-lg" src={img} fill alt="image"/>
             </div>
             <div >
 
